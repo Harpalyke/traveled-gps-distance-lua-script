8d3019ba55805fdd8c9ad3d9efb68e6aa50fae79
Harpalyke <harpalyk3@gmail.com>
2017-11-12 20:56:44 +0000

    Adding a minimum update distance between GPS to stop the total distance going up when the model is sitting in the same place.
    Reading the value of channel 5 (aux channel 1) from the radio and interpret it as arm state of the model.
    
    Avoid increasing the total distance when the model is not armed.
    
    Prevent recreating the TRAV sensor every update so it dosent need ot be rediscovered

--- scripts/telemetry/TRAV.lua
+++ scripts/telemetry/TRAV.lua
@@ -1,7 +1,20 @@
+-- CONFIG PARAMS -- 
+
+-- sets the minimum distance in meters the last logged GPS reading and the current reading before
+-- the difference is added to thetotal distance. This prevents jitter when the quad is sitting still.
+local MINIMUM_UPDATE_DISTANCE_IN_METERS = 2.0;
+-- the channel aux channel the arm switch is set on
+local RC_ARMING_AUX_CHANNEL_NUM = 1;
+-- the switch value that will arm the model -1024 to 1024
+-- when the script detects that the switch value crosses this threshold it assumes the
+-- model has been armed and the distance counter should be reset
+local RC_ARMING_THRESHOLD = -800;
+
+
+
 local lastRunTS = 0;
 local INTERVAL = 50;
-
-local sensorId = 0x09C7;
+local sensorId = 0xBC76;
 local sensorSubID = 0;
 local sensorInstance = 0;
 local sensorName = "TRAV";
@@ -10,13 +23,7 @@ local GPS_SENSOR_ID = -1;
 local LAST_LOCATION_LAT_VALUE = 0;
 local LAST_LOCATION_LON_VALUE = 0;
 local TOTAL_DISTANCE_TRAVELED = 0;
-
-local menuList = {
-    {
-        t = "reset distance",
-        f = resetDistance
-    }
-}
+local LAST_KNOWN_ARM_STATE = nil;
 
 local function getTelemetryId(name)
      local field = getFieldInfo(name)
@@ -51,8 +58,13 @@ local function getTelemetryId(name)
 end
 
 local function writeValueToDistanceSensor(value) 
-  local UNIT_METERS = 9;
-  setTelemetryValue(sensorId, sensorSubID, sensorInstance, value, UNIT_METERS, 0, sensorName);
+  local distSensorId = getTelemetryId(sensorName);
+  if distSensorId == nil or distSensorId < 0 then
+    local UNIT_METERS = 9;
+    setTelemetryValue(sensorId, sensorSubID, sensorInstance, value, UNIT_METERS, 0, sensorName);
+  else
+    setTelemetryValue(sensorId, sensorSubID, sensorInstance, value);
+  end
 end
 
 local function resetDistance()
@@ -78,34 +90,65 @@ local function run()
 end
 local function bg()
   if lastRunTS == 0 or lastRunTS + INTERVAL < getTime() then
-    local value = 10;
+
+    -- first 4 channels are for stick inputs, skip these when looking for the input by aux channel number
+    local armChannelReading = getValue(RC_ARMING_AUX_CHANNEL_NUM+4);
+    --print('armChannelReading', armChannelReading);
+    local isArmed = false;
+    -- assume that setting the switch to anything above -800 is arming the model (i.e. switch not low)
+    if armChannelReading > -800 then
+      isArmed = true;
+    end
+    if LAST_KNOWN_ARM_STATE ~= isArmed then
+      -- armstate changed, update tracked state and reset distance if being armed
+      if isArmed ~= nil then
+        -- active state change 
+        if isArmed == true then
+          print("Armed!");
+          resetDistance();
+        else
+          print("Disarmed");
+        end
+      end
+      LAST_KNOWN_ARM_STATE = isArmed;
+    end
+  
     if GSP_SENSOR_ID < 0 then
       GPS_SENSOR_ID = getTelemetryId("GPS");
     end
-    local updatedGpsSensorValue = getValue(GPS_SENSOR_ID);
+    
+    -- only read GPS value if model is armed
+    if LAST_KNOWN_ARM_STATE == true then
+      local updatedGpsSensorValue = getValue(GPS_SENSOR_ID);
 
-    if type(updatedGpsSensorValue) == "table" then
-      if updatedGpsSensorValue["lat"] ~= nil and updatedGpsSensorValue["lon"] ~= nil then
-        local lat = updatedGpsSensorValue["lat"];
-        local lon = updatedGpsSensorValue["lon"];
-        if lat ~= 0 and lon ~= 0 then
-          --print("lat/lon: ", lat, lon);
-          
-          if LAST_LOCATION_LAT_VALUE ~= lat or LAST_LOCATION_LON_VALUE ~= lon then
-            if LAST_LOCATION_LAT_VALUE ~= 0 and LAST_LOCATION_LON_VALUE ~= 0 then
-              local stepDistanceInKm = geo_distance(LAST_LOCATION_LAT_VALUE, LAST_LOCATION_LON_VALUE, lat, lon);
-              local distanceInMeters = stepDistanceInKm * 1000;
-              TOTAL_DISTANCE_TRAVELED = TOTAL_DISTANCE_TRAVELED + distanceInMeters;
-              --print("new distance: ", TOTAL_DISTANCE_TRAVELED);
+      if type(updatedGpsSensorValue) == "table" then
+        if updatedGpsSensorValue["lat"] ~= nil and updatedGpsSensorValue["lon"] ~= nil then
+          local lat = updatedGpsSensorValue["lat"];
+          local lon = updatedGpsSensorValue["lon"];
+          if lat ~= 0 and lon ~= 0 then
+            --print("lat/lon: ", lat, lon);
+            
+            if LAST_LOCATION_LAT_VALUE ~= lat or LAST_LOCATION_LON_VALUE ~= lon then
+              if LAST_LOCATION_LAT_VALUE ~= 0 and LAST_LOCATION_LON_VALUE ~= 0 then
+                local stepDistanceInKm = geo_distance(LAST_LOCATION_LAT_VALUE, LAST_LOCATION_LON_VALUE, lat, lon);
+                local distanceInMeters = stepDistanceInKm * 1000;
+                if distanceInMeters >= MINIMUM_UPDATE_DISTANCE_IN_METERS then
+                  TOTAL_DISTANCE_TRAVELED = TOTAL_DISTANCE_TRAVELED + distanceInMeters;
+                  LAST_LOCATION_LAT_VALUE = lat;
+                  LAST_LOCATION_LON_VALUE = lon;
+                  --print("new distance: ", TOTAL_DISTANCE_TRAVELED);
+                end
+              else
+                LAST_LOCATION_LAT_VALUE = lat;
+                LAST_LOCATION_LON_VALUE = lon;
+              end
             end
-            LAST_LOCATION_LAT_VALUE = lat;
-            LAST_LOCATION_LON_VALUE = lon;
           end
+        else
+          print("lat or lon null in table");
         end
-      else
-        print("lat or lon null in table");
-      end
-    end 
+      end 
+    end
     writeValueToDistanceSensor(TOTAL_DISTANCE_TRAVELED);
     lastRunTS = getTime();
   end
